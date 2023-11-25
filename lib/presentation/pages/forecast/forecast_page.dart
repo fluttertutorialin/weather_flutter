fbf31845d3e3cb9ce5c9d72cf1ad2aced6f196c5
fluttertutorialin <kamal.lakhani56@gmail.com>
2023-11-25 08:51:42 +0530

    forcast static id remove

--- lib/presentation/pages/forecast/forecast_page.dart
+++ lib/presentation/pages/forecast/forecast_page.dart
@@ -48,8 +48,10 @@ class _ForecastState extends State<ForecastPage> {
                               children: [
                                 _currentForecastView(
                                     context: context,
-                                    locationForecastEntity: state.forecastEntity.locationForecastEntity,
-                                    currentForecastEntity: state.forecastEntity.currentForecastEntity),
+                                    locationForecastEntity: state
+                                        .forecastEntity.locationForecastEntity,
+                                    currentForecastEntity: state
+                                        .forecastEntity.currentForecastEntity),
                                 const Gap(18.0),
                                 _fewDayForecastView(
                                     context: context,
@@ -66,18 +68,15 @@ class _ForecastState extends State<ForecastPage> {
 _currentForecastView(
     {required BuildContext context,
     required CurrentForecastEntity? currentForecastEntity,
-    required LocationForecastEntity? locationForecastEntity
-    }) {
-  final CurrentForecastEntity(:conditionForecastEntity) = currentForecastEntity!;
+    required LocationForecastEntity? locationForecastEntity}) {
+  final CurrentForecastEntity(:conditionForecastEntity) =
+      currentForecastEntity!;
   final LocationForecastEntity(:name, :country) = locationForecastEntity!;
 
   return Column(children: [
-    CachedNetworkImage(
-      imageUrl: 'https:${conditionForecastEntity!.icon!}',
-    ),
+    CachedNetworkImage(imageUrl: 'https:${conditionForecastEntity!.icon!}'),
     const Gap(10.0),
-    Text('$name $country',
-        style: context.labelMediumStyle),
+    Text('$name $country', style: context.labelMediumStyle),
     const Gap(5.0),
     Text(currentForecastEntity.conditionForecastEntity!.text!,
         style: context.labelLargeStyle!.copyWith(fontSize: 18)),
